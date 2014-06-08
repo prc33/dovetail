a829bbc919f97bfa036460744f4d404b344878d3
Pete Calvert <Peter.Calvert@cl.cam.ac.uk>
2014-06-08 17:52:38 +0100

    Modify rand_* runtime utils to allow fairer performance compared to C's rand_r.

--- runtime/util_c.c
+++ runtime/util_c.c
@@ -1,22 +1,20 @@
 #define MAX_RAND   2014
 
-static int rand_ranges[MAX_RAND];
 static unsigned int rand_seeds[MAX_RAND];
 static int rand_length = 0;
 
-int rand_init(int range) {
+int rand_init() {
   int id;
 
   do {
     id = rand_length;
   } while(!llvm_cas(&rand_length, id, id + 1));
 
-  rand_ranges[id] = range;
   rand_seeds[id] = id;
 
   return id;
 }
 
-int rand_next(int id) {
-  return rand_r(&rand_seeds[id]) % rand_ranges[id];
+int rand_next(int id, int max) {
+  return rand_r(&rand_seeds[id]) % max;
 }
